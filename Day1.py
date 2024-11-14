1cf186fc40644b4fd9b0ab01affdf139ea306886
abhiyanShrestha-cybersecurityexpert <abhiyanshrestha105@gmail.com>
2024-11-14 11:09:11 +0545

    Day 1 and Day 2 Python Revision
    day 1 Print Statement and assignment in day 2

---
+++ Day1.py
@@ -0,0 +1,5 @@
+# Simple syntax for printing output
+# Single line
+# Done By Abhiyan Shrestha
+
+print("Hello World !") # # single line syntax for printing Hello World  in this program
---
+++ Day2.py
@@ -0,0 +1,17 @@
+# assignment 1 - store any data in variable and print 10 times
+# single line
+# Done By Abhiyan Shrestha
+
+a = '10' # Storing a as variable 10 as data
+
+# Print Statement
+
+print(a) # output will be printed 10
+print(a) # output will be printed 10 
+print(a) # output will be printed 10
+print(a) # output will be printed 10
+print(a) # output will be printed 10
+print(a) # output will be printed 10
+print(a) # output will be printed 10
+print(a) # output will be printed 10
+print(a) # output will be printed 10
